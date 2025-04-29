302905b0f63ee5dd798a38525e4e3a44ebcb6310
Sulayman <developer.sulayman@gmail.com>
2025-04-29 19:23:48 +0600

    sign up and sign in button added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const Home = () => {
+    const singupWithFirebase = ()=>{
+        
+    }
+    const singinWithFirebase = ()=>{
+
+    }
   return (
-    <div>Home</div>
+    <div className='flex items-center justify-between gap-5'>
+        <button className='btn btn-primary' onClick={()=>singupWithFirebase()}>Sign up</button>
+        <button className='btn btn-warning' onClick={()=>singinWithFirebase()}>Sign in</button>
+       
+    </div>
   )
 }
 
